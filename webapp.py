dc9dd328f8a8ad71129571c27d338b9059a4d4b8
Erik Y <erikyan22@gmail.com>
2018-11-26 12:42:09 -0800

    added signup page route

--- webapp.py
+++ webapp.py
@@ -2,9 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def main():
-    return render_template('index.html')
+    return render_template("index.html")
+
+@app.route("/signUp")
+def showSignUp():
+    return render_template("signUp.html")
+
 
 if __name__ == "__main__":
     app.run(port=5002, debug=True)
