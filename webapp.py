ca4147c708d425e3944d0eb58edbbd63698272da
Erik Y <erikyan22@gmail.com>
2018-11-28 13:36:32 -0800

    added mysql database connection and attempting to connect web app, mysql server, and html using js.

--- webapp.py
+++ webapp.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json, request, redirect, session
+from flaskext.mysql import MySQL
+from werkzeug import generate_password_hash, check_password_hash
 
+mysql = MySQL()
 app = Flask(__name__)
+
+# randomly generated encryption & decryption key, ensures security of a communications session
+app.secret_key = 'captain knuckles?'
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'ohgodwhyisthis!'
+app.config['MYSQL_DATABASE_DB'] = 'milestone'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 @app.route("/")
 def main():
@@ -10,6 +22,42 @@ def main():
 def showSignUp():
     return render_template("signUp.html")
 
+@app.route('/signUp',methods=['POST','GET'])
+def signUp():
+    try:
+        _name = request.form['inputName']
+        _email = request.form['inputEmail']
+        _password = request.form['inputPassword']
 
+        # validates input values
+        if _name and _email and _password:
+
+            # If OK, will call MySQL
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            _hashed_password = generate_password_hash(_password)
+            cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return json.dumps({'message':'User created successfully !'})
+            else:
+                return json.dumps({'error':str(data[0])})
+        else:
+            return json.dumps({'html':'<span>Enter the required fields</span>'})
+
+    except Exception as e:
+        return json.dumps({'error':str(e)})
+
+    finally:
+        cursor.close()
+        conn.close()
+
+
+# =====================================================
+# FOR REALTIME DEBUGGING AND UPDATE WITHOUT APP RESTART
 if __name__ == "__main__":
     app.run(port=5002, debug=True)
+# =====================================================
