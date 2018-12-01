6c9b76d910d9ced4b04fa82a3aefc23eced7f309
Erik Y <erikyan22@gmail.com>
2018-11-30 20:44:36 -0500

    changed json.dump to jsonify to produce flask .response() objects.

--- webapp.py
+++ webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, json, request, redirect, session
+from flask import Flask, render_template, json, request, redirect, session, jsonify
 from flaskext.mysql import MySQL
 from werkzeug import generate_password_hash, check_password_hash
 
@@ -6,12 +6,12 @@ mysql = MySQL()
 app = Flask(__name__)
 
 # randomly generated encryption & decryption key, ensures security of a communications session
-app.secret_key = 'captain knuckles?'
+app.secret_key = "captain knuckles?"
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'ohgodwhyisthis!'
-app.config['MYSQL_DATABASE_DB'] = 'milestone'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config["MYSQL_DATABASE_USER"] = "root"
+app.config["MYSQL_DATABASE_PASSWORD"] = "ohgodwhyisthis!"
+app.config["MYSQL_DATABASE_DB"] = "milestone"
+app.config["MYSQL_DATABASE_HOST"] = "localhost"
 mysql.init_app(app)
 # ==============================================================================
 
@@ -25,34 +25,31 @@ def showSignUp():
 
 # ==============================================================================
 # SIGN UP
-@app.route('/signUp',methods=['POST','GET'])
+@app.route("/signUp",methods=["POST","GET"])
 def signUp():
     try:
-        _name = request.form['inputName']
-        _email = request.form['inputEmail']
-        _password = request.form['inputPassword']
+        _name = request.form["inputName"]
+        _email = request.form["inputEmail"]
+        _password = request.form["inputPassword"]
 
         # validates input values
         if _name and _email and _password:
-
-            # If OK, will call MySQL
-
             conn = mysql.connect()
             cursor = conn.cursor()
             _hashed_password = generate_password_hash(_password)
-            cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+            cursor.callproc("sp_createUser",(_name,_email,_hashed_password))
             data = cursor.fetchall()
 
             if len(data) is 0:
                 conn.commit()
-                return json.dumps({'message':'User created successfully !'})
+                return jsonify({"message":"User created successfully !"})
             else:
-                return json.dumps({'error':str(data[0])})
+                return jsonify({"error":"User already exists !"})
         else:
-            return json.dumps({'html':'<span>Enter the required fields</span>'})
+            return jsonify({"error":"Enter all required fields"})
 
     except Exception as e:
-        return json.dumps({'error':str(e)})
+        return jsonify({"error":str(e)})
 
     finally:
         cursor.close()
